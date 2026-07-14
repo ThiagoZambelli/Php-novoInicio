b00e40824d64eb08bad240d0d4a2c014cb39f975
Thiago Zambelli de Oliveira <thiago.zambelli@santacasa.org.br>
2026-07-14 16:56:21 -0300

    Adiciona arquivos do projeto

---
+++ screen-match.php
@@ -0,0 +1,11 @@
+<?php
+echo "Bem-vindo(a) ao screen match!
+";
+
+$nomeFilme = "Top Gun - Maverick";
+$anoLancamento = 2022;
+$somaDeNotas = 9 + 6 + 8 + 7.5 + 5;
+$notaFilme = $somaDeNotas / 5;
+$incluidoNoPlano = true;
+
+echo $notaFilme;

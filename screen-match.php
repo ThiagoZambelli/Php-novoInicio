90bd21a50f6158e62abb31284198559b075e1428
Thiago Zambelli de Oliveira <thiago.zambelli@santacasa.org.br>
2026-07-14 17:05:59 -0300

    testes de interpolação de varavel

--- screen-match.php
+++ screen-match.php
@@ -1,6 +1,5 @@
 <?php
-echo "Bem-vindo(a) ao screen match!
-";
+echo "\tBem-vindo(a) ao screen match!\n";
 
 $nomeFilme = "Top Gun - Maverick";
 $anoLancamento = 2022;
@@ -8,4 +7,7 @@ $somaDeNotas = 9 + 6 + 8 + 7.5 + 5;
 $notaFilme = $somaDeNotas / 5;
 $incluidoNoPlano = true;
 
-echo $notaFilme;
+
+
+echo "Nome do Filme: " . $nomeFilme . "\n";
+echo "Nota Filme: " . $notaFilme;

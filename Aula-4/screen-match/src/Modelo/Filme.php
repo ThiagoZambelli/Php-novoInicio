41e0c5c09cd7f0f6b4d28f0943cc6c0ba4393b4c
Thiago Zambelli <thiagozamoliveira@gmail.com>
2026-07-17 13:44:09 -0300

    introdução a classes abstratas

---
+++ Aula-4/screen-match/src/Modelo/Filme.php
@@ -0,0 +1,20 @@
+<?php
+
+class Filme extends Titulo
+{
+
+    public function __construct(
+        int $anoLancamento,
+        string $nome,
+        Genero $genero,
+        public readonly int $duracaoEmMinutos
+    ) {
+        parent::__construct(nome: $nome, anoLancamento: $anoLancamento, genero: $genero);
+    }
+
+    public function duracaoEmMinutos(): int
+    {
+        return $this->duracaoEmMinutos;
+    }
+}
+;
---
+++ Aula-4/screen-match/src/Modelo/Serie.php
@@ -0,0 +1,23 @@
+<?php
+
+class Serie extends Titulo
+{
+
+    public function __construct(
+        int $anoLancamento,
+        string $nome,
+        Genero $genero,
+        public readonly int $temporadas,
+        public readonly int $episodiosPorTemporada,
+        public readonly int $minutosPorEpisodio
+    ) {
+        parent::__construct(nome: $nome, anoLancamento: $anoLancamento, genero: $genero);
+    }
+
+    public function duracaoEmMinutos(): int
+    {
+        $episodiosTotais = $this->temporadas * $this->episodiosPorTemporada;
+
+        return $duracaoEmMinutos = $episodiosTotais * $this->minutosPorEpisodio;
+    }
+}

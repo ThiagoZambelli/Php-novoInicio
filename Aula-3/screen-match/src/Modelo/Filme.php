ddae525bd7326fee867e8f329c2542c9744c054d
Thiago Zambelli <thiagozamoliveira@gmail.com>
2026-07-17 08:50:52 -0300

    entendendo definições de privado e publico em classes

--- Aula-3/screen-match/src/Modelo/Filme.php
+++ Aula-3/screen-match/src/Modelo/Filme.php
@@ -4,13 +4,13 @@ class Filme {
     public string $nome;
     public int $anoLancamento;
     public string $genero;
-    public array $notas = [];
+    private array $notas = [];
 
-    function avalia(float $nota): void {
+    public function avalia(float $nota): void {
         $this -> notas[] = $nota;
     }
 
-    function media(): float {
+    public function media(): float {
         $somaNotas = array_sum($this -> notas);
         $quantidadeNotas = count($this -> notas);
 

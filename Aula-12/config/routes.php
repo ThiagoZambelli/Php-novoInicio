3ae85eb96081b52c871c6f2604c495b5860edcae
Thiago Zambelli Oliveira <90356970+ThiagoZambelli@users.noreply.github.com>
2026-08-04 18:11:03 +0000

    ajuste do ambiente para inicio da aula 12

---
+++ Aula-12/config/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'GET|/' => \Aula12\Mvc\Controller\VideoListController::class,
+    'GET|/novo-video' => \Aula12\Mvc\Controller\VideoFormController::class,
+    'POST|/novo-video' => \Aula12\Mvc\Controller\NewVideoController::class,
+    'GET|/editar-video' => \Aula12\Mvc\Controller\VideoFormController::class,
+    'POST|/editar-video' => \Aula12\Mvc\Controller\EditVideoController::class,
+    'GET|/remover-video' => \Aula12\Mvc\Controller\DeleteVideoController::class,
+];
---
+++ Aula-12/src/Controller/Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aula12\Mvc\Controller;
+
+interface Controller
+{
+    public function processaRequisicao(): void;
+}
